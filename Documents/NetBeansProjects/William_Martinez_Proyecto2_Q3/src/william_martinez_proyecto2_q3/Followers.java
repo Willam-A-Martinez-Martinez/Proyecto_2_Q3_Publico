252b8e21c2467f907d84fd672bf300187a188a6c
Miguelspong34 <92834774+Miguelspong34@users.noreply.github.com>
2024-09-11 15:19:53 -0600

    Add files via upload
    Aqui estan los updates de following y followers.

--- Documents/NetBeansProjects/William_Martinez_Proyecto2_Q3/src/william_martinez_proyecto2_q3/Followers.java
+++ Documents/NetBeansProjects/William_Martinez_Proyecto2_Q3/src/william_martinez_proyecto2_q3/Followers.java
@@ -1,24 +1,59 @@
 package william_martinez_proyecto2_q3;
 
-
-
 public class Followers {
-//        Pagina_Inicial pgInicial = new Pagina_Inicial();
+    
 
-//        int contador=0;
-//        User_info[] listaUsuarios = pgInicial.manejoUser.getLista();
-        //cuando apriete un boton (supongo) del usuario de seguir necesito que agrege el nombre del cual sigue y ademas de eso un contador
+    private String[] followers;
+    private int followerCount;
 
-//        public void follower(String nombreSeguidor){
-//            for(int i=0; i>listaUsuarios.length;i++){
-//                if(listaUsuarios[i].getUsername().equals(nombreSeguidor)){
-//                    
-//                }
-//            }
-//        }
+    public Followers() {
+        this.followers= new String[25]; 
+        this.followerCount=0;
+    }
 
-        public void agregarFollower(String username){
-            
+    public void addFollower(String username) {
+        if (followerCount < followers.length) { // Validamos si hay espacio
+            if (!isFollower(username)) { // Verifica si ya existe el seguidor
+                followers[followerCount] = username;
+                followerCount++; // Aumenta el contador de seguidores
+                System.out.println(username + " ahora te sigue.");
+            } else {
+                System.out.println(username + " ya es tu seguidor.");
+            }
+        } else {
+            System.out.println("No puedes tener más seguidores, el límite está lleno.");
         }
-        
+    }
+     
+   // Método para validar si el usuario ya es un seguidor
+   public boolean isFollower(String Username) {
+          for (int i = 0; i < followerCount; i++) {
+            if (followers[i].equals(Username)) {
+                return true; // Ya es seguidor
+            }
+        }
+        return false;
+    }
+     // Método para obtener la lista de seguidores
+    public String[] getFollowersList() {
+        String[] currentFollowers = new String[followerCount];
+        for (int i = 0; i < followerCount; i++) {
+            currentFollowers[i] = followers[i];
+        }
+        return currentFollowers; // Devuelve una copia de la lista actual de seguidores
+    }
+    //Método para mostrar la lista de seguidores
+      public void showFollowers() {
+        if (followerCount == 0) {
+            System.out.println("No tienes seguidores.");
+        } else {
+            System.out.println("Tus seguidores son:");
+            for (int i = 0; i < followerCount; i++) {
+                System.out.println("- " + followers[i]);
+            }
+        }
+    }
 }
+    
+    
+
